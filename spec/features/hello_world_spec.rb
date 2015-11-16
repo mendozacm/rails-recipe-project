59fbd75d465c5d76b8618da6bba31d04f99b68e3
Jordan Hudgens <jordan@wow.com>
2015-11-16 16:38:12 -0600

    Updated test to ensure H1 is checked

--- spec/features/hello_world_spec.rb
+++ spec/features/hello_world_spec.rb
@@ -4,6 +4,6 @@ feature 'routing is working properly' do
 
   scenario 'on the /hello_world page' do
     visit hello_world_path
-    expect(page).to have_content('Hello World')
+    expect(page).to have_css("h1", text: "Hello World")
   end
 end
