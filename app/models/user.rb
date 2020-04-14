63a51140a878a0143ea0b38610b3b0e5eb370d43
cesar_mendoza <mendoza.cesar92@gmail.com>
2020-04-13 20:24:09 -0500

    added validations to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
 
+    validates :name, presence: true
+
+    validates :username, length: {minimum: 5 }, presence: true, uniqueness: true
+
+    validates :bio, length: { maximum: 500 }, presence: true
+
+
     has_many :comments
     has_many :recipes
     has_many :recipes, through: :comments
