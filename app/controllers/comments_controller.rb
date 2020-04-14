eccd7fc37ec4b5ab3ec9f89de843f377a1ef3718
cesar_mendoza <mendoza.cesar92@gmail.com>
2020-04-14 17:25:40 -0500

    added a comment view and form submission

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,28 @@
 class CommentsController < ApplicationController
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
   def index
+   
+    @comments = Comment.all
   end
 
   def show
+      @comment = Comment.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @comment = Comment.create(comment_params)
+    
+    redirect_to comment_path(@comment)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :username, :body, :user_id)
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,11 @@
 Rails.application.routes.draw do
  
 
-  resources :users, only: [:index, :create, :new]
+  resources :users, only: [:index, :create, :new, :show, :update]
 
-  get 'recipes/create'
+  resources :recipes, only: [:index, :create, :new, :show, :update]
 
-  get 'recipes/edit'
-
-  get 'recipes/update'
-
-  get 'recipes/destroy'
-
-  get 'recipes/index'
-
-  get 'recipes/show'
-
-  get 'comments/create'
-
-  get 'comments/edit'
-
-  get 'comments/update'
-
-  get 'comments/destroy'
-
-  get 'comments/index'
-
-  get 'comments/show'
+ resources :comments, only: [:index, :create, :new, :show, :update]
 
   get 'hello_world', to: 'static#hello_world'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
