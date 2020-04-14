7d0fdb5d23121f5c2093fb3d730866fa52a4f616
cesar_mendoza <mendoza.cesar92@gmail.com>
2020-04-14 16:41:28 -0500

    added password confirmation so that has_secure_password works now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,15 +9,19 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
   end
 
-  def new; end
+  def new
+  end
 
   def create
-    @user = User.new
-    @user[:name] = params[:user][:name]
-    @user[:username] = params[:user][:username]
-    @user[:bio] = params[:user][:bio]
-    @user[:password] = params[:user][:password]
-    @user.save
+    @user = User.create(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
     redirect_to users_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :bio, :username, :password, :password_confirmation)
   end
 end
