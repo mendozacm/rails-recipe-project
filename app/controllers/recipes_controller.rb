4488eb4a1a6fdc6d74443bed58feba0dcdf6c55f
cesar_mendoza <mendoza.cesar92@gmail.com>
2020-04-14 22:20:22 -0500

    added an edit and delete form to recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,6 +17,22 @@ class RecipesController < ApplicationController
     redirect_to recipe_path(@recipe)
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+
+    redirect_to recipe_path(@recipe)
+  end
+
+  def destroy
+    Recipe.find(params[:id]).destroy
+    redirect_to "/recipes"
+  end
+
   private
 
   def recipe_params
