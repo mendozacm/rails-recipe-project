6f8f7e362108545ac7e5166d958b911e5ed8986a
cesar_mendoza <mendoza.cesar92@gmail.com>
2020-04-14 22:32:03 -0500

    added a edit and destroy form and methhods to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,22 @@ class CommentsController < ApplicationController
     redirect_to comment_path(@comment)
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+
+    redirect_to comment_path(@comment)
+  end
+
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to "/comments"
+  end
+
   private
 
   def comment_params
